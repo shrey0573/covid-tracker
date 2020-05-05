42326e2c464e992f2afec5a2441391133325b00d
Shrey Chauhan <shreychauhan0573@gmail.com>
2020-05-05 13:06:07 +0530

    Updates

---
+++ precache-manifest.55767779d3ff5cbc0a71153fb109cd68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f9c9cf3f6bb2f65a8b434be6b041f78",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "f6220fff43a7edc551d5",
+    "url": "/covid-tracker/static/css/main.377d5ff4.chunk.css"
+  },
+  {
+    "revision": "8caceac0f673ebbb9a08",
+    "url": "/covid-tracker/static/js/2.0363bbc3.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.0363bbc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6220fff43a7edc551d5",
+    "url": "/covid-tracker/static/js/main.cc19981e.chunk.js"
+  },
+  {
+    "revision": "f210d2661cd825de531c",
+    "url": "/covid-tracker/static/js/runtime-main.b82cc920.js"
+  }
+]);
